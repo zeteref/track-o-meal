1ad9b0a58066a68c0fcd9c24e2f851550d7b03c4
z <z@example.com>
2018-08-31 13:45:49 +0200

    work in progress

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { MealsComponent } from './meals/meals.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { MealDetailComponent } from './meal-detail/meal-detail.component';
 import { IngredientsComponent } from './ingredients/ingredients.component';
+import { NgbootstrapExampleComponent } from './ngbootstrap-example/ngbootstrap-example.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'meals', component: MealsComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'meals/:id', component: MealDetailComponent },
+  { path: 'example', component: NgbootstrapExampleComponent },
   { path: 'ingredients', component: IngredientsComponent }
 ];
 
